e6a4c613d6058179bfa1690511f73ac7caaf6bee
bhanu <24r11a6648@gcet.edu.in>
2026-07-24 18:40:20 +0530

    Fix mobile auth register flow and Android release build

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.namichat_lite"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
